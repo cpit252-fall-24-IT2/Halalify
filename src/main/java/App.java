5ea3b51ad0c46afdc34369377e16634f93007bf5
SaaDN1 <saadnahas1@gmail.com>
2024-11-03 22:30:41 +0300

    Added VideoProcessor class and implemented the video upload method.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,14 @@
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
 
+        VideoProcessor videoProcessor = new VideoProcessor();
+
+        File videoFile = videoProcessor.uploadVideo();
+        if (videoFile == null) {
+            System.err.println("No video selected. Exiting.");;
+            System.exit(1);
+        }
     }
 }
